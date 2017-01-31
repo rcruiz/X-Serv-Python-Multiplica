f3d29a821faef524f7ce3a3c15f6621d6b24f2c9
rcruiz <rc.ruiz@alumnos.urjc.es>
2017-01-31 21:03:08 +0100

    Hecho

--- multiplica.py
+++ multiplica.py
@@ -1,7 +1,7 @@
 #!-/usr/bin/python3
 
-for i in range(1,11):
+for i in range(1, 11):
     Titulo = "Tabla del " + str(i)
-    print Titulo + '\n' + '-' * len(Titulo)
-    for j in range(1,11):
-        print str(i) + ' por ' + str(j) + ' es ' + str(i*j)
+    print(Titulo + '\n' + '-' * len(Titulo))
+    for j in range(1, 11):
+        print(str(i) + ' por ' + str(j) + ' es ' + str(i*j))
